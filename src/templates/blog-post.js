1ed96dc758ffdb11f9f5800233e647a9a70c3192
Bartosz Wójtowicz <sunfireproject@gmail.com>
2019-06-20 15:13:45 +0200

    tag problem

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,42 +1,46 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import get from 'lodash/get'
-import Img from 'gatsby-image'
+import React from "react";
+import Helmet from "react-helmet";
+import get from "lodash/get";
+import Img from "gatsby-image";
 
-import heroStyles from '../components/hero.module.css'
+import heroStyles from "../components/hero.module.css";
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = get(this.props, 'data.contentfulBlogPost')
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const post = get(this.props, "data.contentfulBlogPost");
+    const siteTitle = get(this.props, "data.site.siteMetadata.title");
 
     return (
-      <div style={{ background: '#fff' }}>
+      <div style={{ background: "#fff" }}>
         <Helmet title={`${post.title} | ${siteTitle}`} />
         <div className={heroStyles.hero}>
-          <Img className={heroStyles.heroImage} alt={post.title} sizes={post.heroImage.sizes} />
+          <Img
+            className={heroStyles.heroImage}
+            alt={post.title}
+            sizes={post.heroImage.sizes}
+          />
         </div>
         <div className="wrapper">
           <h1 className="section-headline">{post.title}</h1>
           <p
             style={{
-              display: 'block',
+              display: "block"
             }}
           >
             {post.publishDate}
           </p>
           <div
             dangerouslySetInnerHTML={{
-              __html: post.body.childMarkdownRemark.html,
+              __html: post.body.childMarkdownRemark.html
             }}
           />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default BlogPostTemplate
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
